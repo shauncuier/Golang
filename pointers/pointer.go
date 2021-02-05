e548db830c8cb484278080f886bdac6e337c4d9b
Shaun <shauncuier@gmail.com>
2021-02-05 12:00:54 +0600

    Git pointer

---
+++ pointers/pointer.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+    a := 5
+	b := &a
+
+	fmt.Println(a,"\n",b)
+	fmt.Printf("%T\n", a)
+	fmt.Printf("%T\n", b)
+
+	// Use * to read value from address
+
+	fmt.Println(b)
+	fmt.Println(*b)
+	fmt.Println(*&a)
+
+	// Change value with pointer
+
+	*b =10
+	fmt.Println(a)
+}
